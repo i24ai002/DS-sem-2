f871975f4d7a2bb5f8deea982791dd996754f33d
i24ai002 <i24ai002@aid.svnit.ac.in>
2025-02-09 22:05:47 +0530

    Add files via upload

---
+++ Assignment 5/program 2.c
@@ -0,0 +1,130 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 100  
+
+int stack[N];
+int top = -1;
+
+void push()
+{
+    int x;
+    printf("Enter the value to be pushed: ");   
+    scanf("%d", &x);
+    if(top >= N-1)
+    {
+        printf("Stack Overflow\n");
+    }
+    else
+    {
+        top++;
+        stack[top] = x;
+        printf("%d pushed to stack\n", x);
+    }
+}
+
+void pop()
+{
+    if(top == -1)
+    {
+        printf("Stack Underflow\n");
+    }
+    else
+    {
+        printf("%d popped from stack\n", stack[top]);
+        top--;
+    }
+}
+
+void peek()
+{
+    if(top == -1)
+    {   
+        printf("Stack Underflow\n");
+    }
+    else
+    {
+        printf("Top element is %d\n", stack[top]);
+    }
+}
+
+void display()
+{
+    if(top == -1)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("Stack elements are: ");
+        for(int i=top; i>=0; i--)
+        {
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    }
+}
+
+void isFull()
+{
+    if(top >= N-1)
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+}
+
+void isEmpty()
+{
+    if(top <= N-1)
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+}
+
+int main()
+{
+    int choice;
+
+    do 
+    {
+        printf("Enter 1.push  2.pop  3.peek  4.display  5.check if stack is full  6.check if stack is empty  7.exit: ");
+        scanf("%d", &choice);
+
+        switch(choice) 
+        {
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                isFull();
+                break;
+            case 6:
+                isEmpty();
+                break;
+            case 7:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice\n");
+        }
+    } 
+    while(choice != 7);
+    
+    return 0;
+}
